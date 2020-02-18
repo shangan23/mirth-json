2e4740119d95a953e32f51888815f89c06a3fb3d
shankarganesh.j <shankarganesh.j@ptpll264.payoda.com>
2020-02-18 20:26:00 +0530

    Mapper json support enhanced.

--- src/main/java/JsonUtilities/App.java
+++ src/main/java/JsonUtilities/App.java
@@ -3,13 +3,13 @@ package JsonUtilities;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
-
-import net.minidev.json.JSONArray;
-
 
 /**
  * Hello world!
@@ -17,59 +17,79 @@ import net.minidev.json.JSONArray;
  */
 public class App {
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		String customReader = readFile("/home/shankarganesh.j/mirth/mirth-fhir-utility/src/main/java/JsonUtilities/JsonFiles/custom.json");
-		String fhirReader = readFile("/home/shankarganesh.j/mirth/mirth-fhir-utility/src/main/java/JsonUtilities/JsonFiles/fhirR4.json");
-		
+
+		String basePath = "/home/shankarganesh.j/mirth/mirth-fhir-utility/src/main/java/JsonUtilities/JsonFiles/";
+
+		String customReader = readFile(basePath + "custom.json");
+		String fhirReader = readFile(basePath + "fhirR4.json");
+		String mapReader = readFile(basePath + "mapper.json");
+
 		Object customJson = JsonPath.parse(customReader).json();
 		Object fhirJson = JsonPath.parse(fhirReader).json();
-		//String fhirToParse = fhirJson.toString();
-		//List fhirField = JsonPath.read(fhirJson, "$.category");
-		
-		System.out.println(JsonPath.read(customJson, "$.Data[0].Data[?(@.FieldId=='2202')].Value").getClass().getSimpleName());
+		Object mapJson = JsonPath.parse(mapReader).json();
 		DocumentContext fhir = JsonPath.parse(fhirJson);
-		fhir = fhir.set("$.category", JsonPath.read(customJson, "$.Data[0].Data[?(@.FieldId=='2201')].Value"));
-		if(JsonPath.read(fhirJson, "$.note[0].text") instanceof String){
-			String t = JsonPath.read(customJson, "$.Data[0].Data[?(@.FieldId=='2202')].Value").toString();
-			t = t.substring(1, t.length()-1);
-			t= t.substring(1, t.length()-1);  
-			fhir = fhir.set("$.note[0].text", t);
-		}
-		System.out.println(fhir.jsonString());
-	    
-	    /*String json = fhirAllergy();
-		Object updatedJson = JsonPath.parse(json).json();
-		List category = JsonPath.read(updatedJson, "$.entry[*].resource.category");
-		List clinicalStatus = JsonPath.read(updatedJson, "$.entry[*].resource.clinicalStatus.coding[0].code");
-		String jsonToParse = updatedJson.toString();
-		// json = JsonPath.parse(jsonToParse).set("$.entry[1].resource.category",
-		// "food");
-		// [*][Data][0][0].InterfaceData[?FieldId=='2254'][].Value
-		List ncategory = JsonPath.read(json, "$.entry[*].resource.category");
-		System.out.println(category);
-		System.out.println(clinicalStatus);
-		System.out.println(ncategory);
 
-		String cstm = customJson();
-		Object jsonObj = JsonPath.parse(cstm).json();
-		List Field = JsonPath.read(jsonObj, "$.[*].Data[0].InterfaceData[?(@.FieldId=='2254')].Value");
-		System.out.println(Field);*/
+		LinkedHashMap<String, String> lhm = (LinkedHashMap<String, String>) mapJson;
+		Set set = lhm.entrySet();
+		Iterator iterator = set.iterator();
+		while (iterator.hasNext()) {
+			Map.Entry me = (Map.Entry) iterator.next();
+			fhir = setJson(fhir,customJson,fhirJson,me.getKey().toString(),me.getValue().toString());
+		}
+		
+		//Object resultJson = JsonPath.
+		System.out.println(fhir.jsonString());
+
+		// mapJson.keySet().forEach(key -> System.out.println(key));
+		// mapJson.entrySet().forEach(entry -> System.out.println(entry.getKey()));
+
+		// typeOf(JsonPath.read(customJson,
+		// "$.Data[0].Data[?(@.FieldId=='2202')].Value").getClass().getSimpleName());
+		/*fhir = fhir.set("$.category", JsonPath.read(customJson, "$.Data[0].Data[?(@.FieldId=='2201')].Value"));
+		if (JsonPath.read(fhirJson, "$.note[0].text") instanceof String) {
+			fhir = fhir.set("$.note[0].text",
+					convertToString(JsonPath.read(customJson, "$.Data[0].Data[?(@.FieldId=='2202')].Value")));
+		}
+		System.out.println(fhir.jsonString());*/
 	}
 	
+	public static DocumentContext setJson(DocumentContext fhir, Object customJson, Object fhirJson, String key, String val) {
+		System.out.println("Instance Type >> "+JsonPath.read(fhirJson, key).getClass().getSimpleName());
+		if (JsonPath.read(fhirJson, key) instanceof String) {
+			fhir = fhir.set(key,
+					convertToString(JsonPath.read(customJson, val)));
+		}else {
+			fhir = fhir.set(key, JsonPath.read(customJson, val));
+		}
+		return fhir;
+	}
+
 	public static String readFile(String filename) {
-	    String result = "";
-	    try {
-	        BufferedReader br = new BufferedReader(new FileReader(filename));
-	        StringBuilder sb = new StringBuilder();
-	        String line = br.readLine();
-	        while (line != null) {
-	            sb.append(line);
-	            line = br.readLine();
-	        }
-	        result = sb.toString();
-	    } catch(Exception e) {
-	        e.printStackTrace();
-	    }
-	    return result;
+		String result = "";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filename));
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				line = br.readLine();
+			}
+			result = sb.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	private static String convertToString(Object object) {
+		String t = object.toString();
+		t = t.substring(1, t.length() - 1);
+		t = t.substring(1, t.length() - 1);
+		return t;
+	}
+
+	public static <T> String typeOf(T o) {
+		System.out.println(o.getClass().getSimpleName());
+		return o.getClass().getSimpleName();
 	}
 }
